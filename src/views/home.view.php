fc4672828917b7660ef93dddedf1f3fdfba58ded
Sergi Lozano <sergilozano96@gmail.com>
2023-01-12 16:56:07 +0100

    Update home.view.php

--- src/views/home.view.php
+++ src/views/home.view.php
@@ -21,7 +21,7 @@ require 'partials/navhome.view.php';
   <div class="loginmodal hidden">
     <form class="formlogin animate" action="/auth/signin" method='POST'>
       <label for="email">Email</label>
-      <input type="text" name="email" placeholder="youremail@gmail.com" required><br><br>
+      <input type="email" name="email" placeholder="youremail@gmail.com" required><br><br>
       <label for="password">Contraseña</label>
       <input type="password" name="password" placeholder="********" required>
       <p>Recordarme en este equipo</p>
@@ -36,11 +36,11 @@ require 'partials/navhome.view.php';
   <div class="registermodal hidden">
     <form class="formlogin animate" action="/auth/signup" method='POST'>
       <label for="email">Email</label>
-      <input type="text" name="email" placeholder="youremail@gmail.com" required><br><br>
+      <input type="email" name="email" placeholder="youremail@gmail.com" required><br><br>
       <label for="username">Nombre de usuario</label>
       <input type="text" name="username" placeholder="pepeViyuela" required><br><br>
       <label for="phone">Telèfon</label>
-      <input type="text" name="phone" placeholder="666555444" required><br><br>
+      <input type="tel" name="phone" placeholder="666555444" pattern = "[0-9]{9}" required><br><br>
       <label for="password">Contraseña</label>
       <input type="password" name="password" placeholder="1234" required>
       <button type="submit" class="formloginbutton">Registrarse</button>
