4a29a394cd783fbe3f9486909b83a259448c1f7c
Sergi Lozano <sergilozano96@gmail.com>
2023-01-12 20:35:22 +0100

    admin

--- src/views/admin.view.php
+++ src/views/admin.view.php
@@ -1,16 +1,14 @@
 <?php
 require 'partials/header.view.php';
-require 'partials/navhome.view.php';
+//require 'partials/navhome.view.php';
 ?>
 
 <body>
-<div class="nav__links">
-            <li><a id="catalogue__navlink" class="navlink button" href="/dashboard">
-                    Cátalogo</a></li>
-            <li><a class="navlink derecha" href="/dashboard/bookings">Mis reservas</a></li>
-            <li><a class="navlink derecha" href="#">Historial</a></li>
+    <header>
+        <div class="nv">
+
         </div>
-        <div class="caja2">
+        <div class="caja2"> 
             <div class="closesession">
                 <i class="logouticon material-icons">logout</i>
                 <a href="/auth/logout">Cerrar sesion</a>
@@ -19,14 +17,14 @@ require 'partials/navhome.view.php';
     </header>
     <div class="users">
         <div class="user">
-            <img src="ardillaSurf.png" class="fotoUser">
+            <img src="/public/img/ardillaSurf.png" class="fotoUser">
             <div class="datosUser">
                 <p class="nombreUser">perico</p>
                 <p class="rolUser">socio</p>
             </div>
         </div>
         <div class="user">
-            <img src="ardillaTrabajador.jpg" class="fotoUser">
+            <img src="/public/img/ardillaTrabajador.jpg" class="fotoUser">
             <div class="datosUser">
                 <p class="nombreUser">salva</p>
                 <p class="rolUser">trabajador</p>
